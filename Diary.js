ba12c8963074bf2a34cede975ab5c639d4835136
James Mackintosh <jazmac6@gmail.com>
2023-01-16 16:05:07 +0000

    Diary Class

---
+++ Diary.js
@@ -0,0 +1,22 @@
+class Diary {
+
+    lock = true;
+    input;
+    entry;
+
+    unlock() {
+        if (lock === true) {
+            return;
+        }
+        else {
+            this.addEntry(input) = entry;
+
+            this.lookEntry(input) {
+                return this.entry;
+            }
+        }
+    }
+
+
+
+}
